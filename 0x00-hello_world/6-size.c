a4faaa2dc34733a75ffbf8f3803e3be6c27fb757
Nsimaeves45 <udohnsima5@gmail.com>
2023-04-20 21:37:08 -0700

    6-szie

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf('sizeofachar: %lu byte(s)\n;, sizeof(char));
+	printf('sizeofachar: %lu byte(s)\n;, sizeof(char);
 	printf('sizeofanint: %lu byte(s)\n;, sizeof(int));
 	printf('Sizeofalongint: %lu byte(s)\n;, sizeof(long int));
 	printf('Sizeofalonglongint: %lu byte(s)\n;, sizeof(long long int));
