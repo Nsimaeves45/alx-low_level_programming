2f33d02f9c2979c580c2b152dfdf6f91cd5d9346
Nsimaeves45 <udohnsima5@gmail.com>
2023-04-25 09:12:47 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,17 @@
 
 /**
  * print_alphabet_x10 - prints alpha 10 times
- *
- * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
-}
+	int ten;
+	char la;
+	
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			 _putchar(la);
+		_putchar('\n');
+			
+	}
+}	
